fa99b964ac25f77ec118e049f788562fb55cbc4b
ManishaMiranda <miranda34@mail.usf.edu>
2019-09-11 02:22:45 -0400

    Executes for loop

---
+++ OOPProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOPProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            Console.Write("Enter the number of years of experience you have: ");
+            val = Console.ReadLine();
+            int experience = Convert.ToInt32(val);
+            Console.WriteLine("Executing a for loop:\nThis for loop will execute {0} number of times", (experience + 1));
+            for (int i = 0; i <= experience; i++)
+            {
+                Console.WriteLine("You have {0} years of experience", i);
+            }
+        }
+    }
+}
